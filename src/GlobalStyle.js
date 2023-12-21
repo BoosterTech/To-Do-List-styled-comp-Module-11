fa6dbd784ba398c0dc807c1e6cef0b3c9423160d
Dariusz Podczasik <boostertech@mail.com>
2023-12-21 14:42:37 +0000

    GlobalStyles created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+::before,
+::after {
+  box-sizing: inherit;
+}
+
+body {
+  background-color: rgb(221, 215, 215);
+  font-family: 'Montserrat', sans-serif;
+  max-width: 70%;
+  width: 1000px;
+  margin: auto;
+  padding: 15px;
+  font-size: small;
+  ;
+}
+
+@media(max-width: 768px) {
+  body {
+    max-width: 90%;
+  }
+}
+`;
