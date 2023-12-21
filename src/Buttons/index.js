9076a7ae39451b3013b5ba8ba74a056f118514f8
Dariusz Podczasik <boostertech@mail.com>
2023-12-21 21:45:19 +0000

    Button component code organized

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -5,18 +5,13 @@ const Buttons = ({ tasks, hideDoneTasksVar, toggleHideDone, setAllDone }) =>
   tasks.length > 0 && (
     <ButtonsWrapper>
       <Button
-        className="buttons__button"
         onClick={toggleHideDone}
         disabled={!tasks.some(({ done }) => done)}
       >
         {" "}
         {hideDoneTasksVar ? "Pokaż" : "Ukryj"} ukończone{" "}
       </Button>
-      <Button
-        className="buttons__button"
-        disabled={tasks.every(({ done }) => done)}
-        onClick={setAllDone}
-      >
+      <Button disabled={tasks.every(({ done }) => done)} onClick={setAllDone}>
         {" "}
         Ukończ wszystkie{" "}
       </Button>
