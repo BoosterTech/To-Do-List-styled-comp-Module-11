8d409ef1d981ed9e4f7d327fd00315b82d4dee92
Dariusz Podczasik <boostertech@mail.com>
2023-12-25 15:32:02 +0000

    focus to input added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { Button, Input, TaskForm } from "./styled";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+
+    if (!newTaskContent.trim()) {
+      return;
+    }
+
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    inputRef.current.focus();
   };
 
   return (
     <TaskForm onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         type="text"
         placeholder="Co jest do zrobienia?"
